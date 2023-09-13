4cea2d90f06caccfc08a8ac1ccb1cc591e1d7ff2
atgorvi <dghulay41@gmail.com>
2023-09-13 19:49:36 +0300

    Initial commit

---
+++ demo.py
@@ -0,0 +1,24 @@
+import argparse
+
+import cv2
+
+from src.orientation_correction import correct_orientation
+from src.utils import save_from_numpy
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-img", "--image_path", type=str, help="Path to the image.", required=True
+    )
+    parser.add_argument(
+        "-s", "--save_path", type=str, help="Save correction result", required=False, default="correction_result.png"
+    )
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    image = cv2.imread(args.image_path)
+    correction_angle, corrected_image = correct_orientation(image)
+    save_from_numpy(args.save_path, corrected_image)
---
+++ gradio_demo.py
@@ -0,0 +1,19 @@
+import gradio as gr
+import numpy as np
+
+from src.orientation_correction import correct_orientation
+from typing import Tuple
+
+
+def demo_correction(input_image: np.ndarray) -> Tuple[int, np.ndarray]:
+    correction_angle, corrected_image = correct_orientation(input_image)
+    return int(correction_angle), corrected_image
+
+if __name__ == "__main__":
+    demo = gr.Interface(fn=demo_correction,
+                        inputs=gr.inputs.Image(type="numpy"),
+                        outputs=[gr.outputs.Textbox(label="Correction angle"), gr.outputs.Image(type="numpy")],
+                        examples=[["data/images/0.png"]],
+                        )
+
+    demo.launch()
